c4a03278b19e8782fc0993b61e554ce3c589d2e3
Johnattan Fabian Gutierrez <johnattan.gutierrez@flex.com>
2017-11-16 13:15:25 -0800

    [Studio] Add screen "result"

--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.result', {
+        views: {
+            app: {
+                controller: 'app_result',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.result');
+                }
+            }
+        }
     });
 });
